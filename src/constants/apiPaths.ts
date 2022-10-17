b2c1ab669326ea6218016bff23f10bedf27dfcb6
Vlad Meleshko <vmeleshko@xbsoftware.by>
2022-10-17 20:26:49 +0300

    change import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://qj6sehm224.execute-api.eu-west-1.amazonaws.com",
+  import: "https://438tzqd8la.execute-api.eu-west-1.amazonaws.com",
   bff: "https://snqv0i1zn8.execute-api.eu-west-1.amazonaws.com/products",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
