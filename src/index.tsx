677c41318a9c9db058458a093bc99a1cffef6e29
Vlad Meleshko <vmeleshko@xbsoftware.by>
2022-10-22 22:08:09 +0300

    add authorization header for import request and axios interceptor

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from "axios";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +19,21 @@ if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
   worker.start({ onUnhandledRequest: "bypass" });
 }
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const { response } = error;
+
+    if (response.status === 401 || response.status === 403) {
+      alert(
+        `Server responded with ${response.status} status code: ${response.data?.message}`
+      );
+    }
+
+    return Promise.reject(error.response);
+  }
+);
 
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
